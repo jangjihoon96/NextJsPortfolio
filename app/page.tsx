2937079b718537fe3f8cd4e2226d4ec5e8935843
Jihoon <jangna96@gmail.com>
2023-06-06 16:56:47 +0900

    chore: 메인페이지 컴포넌트 분리
    - 큰 범위의 컴포넌트 분리

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,32 @@
 "use client";
-import Image from "next/image";
-import styles from "./page.module.css";
 import styled from "styled-components";
+import Main from "./components/main/main/main";
+import About from "./components/main/about/about";
+import Skills from "./components/main/skills/skills";
+import Portfolio from "./components/main/portfolio/portfolio";
+import Contact from "./components/main/contact/contact";
 
 export default function Home() {
   return (
     <>
-      <StyledTitle>안녕</StyledTitle>
+      <StyledContainer>
+        <StyledContainerInner>
+          <Main />
+          <About />
+          <Skills />
+          <Portfolio />
+          <Contact />
+        </StyledContainerInner>
+      </StyledContainer>
     </>
   );
 }
-const StyledTitle = styled.h1`
-  font-size: 30px;
+const StyledContainer = styled.div`
+  width: 100%;
+  margin-top: 4.375rem;
+`;
+const StyledContainerInner = styled.div`
+  width: 100%;
+  max-width: var(--container-width);
+  margin: 0 auto;
 `;
