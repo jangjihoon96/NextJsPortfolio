5bc5ef1318a8486b2a6af095fc1b0d71be22e173
Jihoon <jangna96@gmail.com>
2023-06-14 21:32:02 +0900

    feat: Contact 서브타이틀 컴포넌트 제작

---
+++ app/components/main/contact/contactSubTitle.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export default function ContactSubTitle({ children }: { children: string }) {
+  return <StyledContactSubTitle>{children}</StyledContactSubTitle>;
+}
+
+const StyledContactSubTitle = styled.h3`
+  font-weight: 500;
+  font-size: var(--text-md);
+  color: var(--dark);
+  text-align: center;
+  margin-bottom: 1.75rem;
+`;
