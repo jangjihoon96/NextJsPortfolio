f27acc8697e5e7508f2abf001f66c27d25ba8b31
Jihoon <jangna96@gmail.com>
2023-06-15 19:35:21 +0900

    feat: Contact Button 컴포넌트 제작

---
+++ app/components/main/contact/contactSendButton.tsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+import { faEnvelope, faRocket } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default function ContactSendButton({
+  type,
+  children,
+}: {
+  type: "submit" | "reset" | "button" | undefined;
+  children: string;
+}) {
+  return (
+    <StyledContactSendButton type={type}>
+      {children}
+      <FontAwesomeIcon icon={faRocket} className="icon" />
+    </StyledContactSendButton>
+  );
+}
+
+const StyledContactSendButton = styled.button`
+  box-sizing: border-box;
+  width: 100%;
+  border: none;
+  margin-top: 1.5rem;
+  padding: 1.75rem 0;
+  background-color: var(--dark);
+  border-radius: 0.75rem;
+  font-weight: 500;
+  font-size: var(--text-sm);
+  color: var(--white);
+  cursor: pointer;
+  &:hover {
+    background-color: var(--black);
+  }
+  .icon {
+    margin-left: 0.5rem;
+  }
+`;
