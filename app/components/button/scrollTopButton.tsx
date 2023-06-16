800643a42bd00a80bddb4e208c2c2287f689b4d7
Jihoon <jangna96@gmail.com>
2023-06-16 20:04:45 +0900

    feat: 스크롤 top 이동 버튼 기능 추가
    - 버튼 클릭시 최상단으로 스크롤 이동
    - 클린업 함수를 추가하여 이벤트 핸들러가 메모리 누수 없이 작동
    - return문에 클린업 함수 반환, 컴포넌트 언마운트시 이벤트 리스너 제거

--- app/components/button/scrollTopButton.tsx
+++ app/components/button/scrollTopButton.tsx
@@ -1,9 +1,42 @@
 "use client";
 import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useEffect } from "react";
 import styled from "styled-components";
 
 export default function ScrollTopButton() {
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop =
+        document.documentElement.scrollTop || document.body.scrollTop;
+      const topButton = document.getElementById("topButton");
+
+      if (topButton) {
+        if (scrollTop > 500) {
+          topButton.style.display = "block";
+        } else {
+          topButton.style.display = "none";
+        }
+      }
+    };
+
+    const goToTopScroll = () => {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    const topButton = document.getElementById("topButton");
+    if (topButton) {
+      topButton.addEventListener("click", goToTopScroll);
+    }
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      if (topButton) {
+        topButton.removeEventListener("click", goToTopScroll);
+      }
+    };
+  }, []);
+
   return (
     <StyledScrollTopButton id="topButton">
       <FontAwesomeIcon icon={faArrowUp} aria-label="맨위로" />
