44cb69adc665dc338829201728cf24fcc431c4be
Jihoon <jangna96@gmail.com>
2023-06-19 13:48:44 +0900

    feat: header 메뉴 클릭시 스크롤 기능 추가
    - 메뉴 클릭시 해당 섹션으로 스크롤 되는 기능 추가
    - 리팩토링 필요

--- app/header.tsx
+++ app/header.tsx
@@ -1,7 +1,77 @@
 "use client";
+import { useEffect } from "react";
 import styled from "styled-components";
 
 export default function Header() {
+  useEffect(() => {
+    const home = document.getElementById("home")?.offsetTop;
+    const about = document.getElementById("about")?.offsetTop;
+    const skills = document.getElementById("skills")?.offsetTop;
+    const qualification = document.getElementById("qualification")?.offsetTop;
+    const portfolio = document.getElementById("portfolio")?.offsetTop;
+    const contact = document.getElementById("contact")?.offsetTop;
+    const moveHome = document.querySelector(".moveHome");
+    const moveAbout = document.querySelector(".moveAbout");
+    const moveSkills = document.querySelector(".moveSkills");
+    const moveQualification = document.querySelector(".moveQualification");
+    const movePortfolio = document.querySelector(".movePortfolio");
+    const moveContact = document.querySelector(".moveContact");
+    const homeScroll = () => {
+      if (home) {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+      }
+    };
+    const aboutScroll = () => {
+      if (about) {
+        window.scrollTo({ top: about - 200, behavior: "smooth" });
+      }
+    };
+    const skillsScroll = () => {
+      if (skills) {
+        window.scrollTo({ top: skills - 200, behavior: "smooth" });
+      }
+    };
+    const qualificationScroll = () => {
+      if (qualification) {
+        window.scrollTo({ top: qualification - 200, behavior: "smooth" });
+      }
+    };
+    const portfolioScroll = () => {
+      if (portfolio) {
+        window.scrollTo({ top: portfolio - 200, behavior: "smooth" });
+      }
+    };
+    const contactScroll = () => {
+      if (contact) {
+        window.scrollTo({ top: contact - 200, behavior: "smooth" });
+      }
+    };
+    moveHome?.addEventListener("click", (e) => {
+      e.preventDefault();
+      homeScroll();
+    });
+    moveAbout?.addEventListener("click", (e) => {
+      e.preventDefault();
+      aboutScroll();
+    });
+    moveSkills?.addEventListener("click", (e) => {
+      e.preventDefault();
+      skillsScroll();
+    });
+    moveQualification?.addEventListener("click", (e) => {
+      e.preventDefault();
+      qualificationScroll();
+    });
+    movePortfolio?.addEventListener("click", (e) => {
+      e.preventDefault();
+      portfolioScroll();
+    });
+    moveContact?.addEventListener("click", (e) => {
+      e.preventDefault();
+      contactScroll();
+    });
+  }, []);
+
   return (
     <StyledHeader>
       <div className="inner">
@@ -10,22 +80,34 @@ export default function Header() {
         </h1>
         <ul className="gnb">
           <li>
-            <a href="#">Home</a>
+            <a href="#" className="moveHome">
+              Home
+            </a>
           </li>
           <li>
-            <a href="#">About</a>
+            <a href="#" className="moveAbout">
+              About
+            </a>
           </li>
           <li>
-            <a href="#">Skills</a>
+            <a href="#" className="moveSkills">
+              Skills
+            </a>
           </li>
           <li>
-            <a href="#">Qualification</a>
+            <a href="#" className="moveQualification">
+              Qualification
+            </a>
           </li>
           <li>
-            <a href="#">Portfolio</a>
+            <a href="#" className="movePortfolio">
+              Portfolio
+            </a>
           </li>
           <li>
-            <a href="#">Contact</a>
+            <a href="#" className="moveContact">
+              Contact
+            </a>
           </li>
         </ul>
       </div>
